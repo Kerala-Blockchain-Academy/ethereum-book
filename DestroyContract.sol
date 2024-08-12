193c6b8c51d367d374e7dd0daafb09d566f62fb4
sumimaria <38939520+sumimaria@users.noreply.github.com>
2024-08-12 09:13:52 +0530

    Update DestroyContract.sol

--- DestroyContract.sol
+++ DestroyContract.sol
@@ -1,1 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+contract Contract {
+   address payable admin;
+   constructor() {
+       admin = payable(msg.sender);
+   }
+   function destroy() public {
+       require(msg.sender==admin,"Unauthorized Operation");
+       selfdestruct(admin);
+   }
+   receive() external payable{
+
+
+   }
+
+
+}
 
