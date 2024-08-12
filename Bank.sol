836df06f775aa454c627d1d24c5717e508011e1b
sumimaria <38939520+sumimaria@users.noreply.github.com>
2024-08-12 09:51:44 +0530

    Added Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.19;
 contract Bank{
    mapping(address=>uint) balanceLedger;//<acc1,bal>, <acc2,bal>,
    mapping(uint=>address) index;
    uint addressCount;
    address owner;
+
+
    event depositEvent(address depositorAcc, uint deposit);
    modifier balanceCheck(uint amt){
        require(amt <= balanceLedger[msg.sender],"Insufficient Balance");
@@ -15,10 +17,13 @@ contract Bank{
        require(msg.sender==owner,"Access denied");
        _;
    }
-   
-  constructor(){ 
-   admin=msg.sender;
-}
+
+
+   constructor(){
+       owner = msg.sender;
+   }
+
+
    function deposit()public payable{
        if(balanceLedger[msg.sender]==0){
            index[addressCount++]=msg.sender; // index mapping key starts from 0
@@ -26,7 +31,9 @@ contract Bank{
        balanceLedger[msg.sender] += msg.value; //shorthand operator
        emit depositEvent(msg.sender,msg.value);
    }
-function withdraw(uint amt)public balanceCheck(amt){
+
+
+   function withdraw(uint amt)public balanceCheck(amt){
        balanceLedger[msg.sender] -= amt;
        payable(msg.sender).transfer(amt);
       
@@ -37,6 +44,8 @@ function withdraw(uint amt)public balanceCheck(amt){
        balanceLedger[msg.sender] -= amt;
        recipient.transfer(amt);
    }
+
+
    function getMyBalance()public view returns(uint){
        return balanceLedger[msg.sender];
    }
@@ -49,6 +58,8 @@ function withdraw(uint amt)public balanceCheck(amt){
    function getBankBalance() public view returns(uint){
        return address(this).balance;
    }
+
+
    function dissolveBank() public onlyOwner(){
        address indexAddress;
        for(uint i=0; i<addressCount;i++){
@@ -62,13 +73,3 @@ function withdraw(uint amt)public balanceCheck(amt){
   
 }
 
-
-
-
-
-
-
-
-   
-
-   
